736fa8da01596a16789a655fb5ca7125b753381e
PeteJames21 <peetjames21@gmail.com>
2024-04-02 10:36:40 +0300

    Implement a basic cache class

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Implement a basic cache class that stores an unlimited number of items
+to an in-memory dictionary.
+"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """A basic cache for storing items to an in-memory dict."""
+    MAX_ITEMS = None
+
+    def put(self, key, item):
+        """Add an item to the cache using the specified key."""
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item from the cache using the specified key."""
+        if key is None:
+            return None
+        return self.cache_data.get(key, None)
---
+++ 0x01-caching/README.md
@@ -0,0 +1,7 @@
+# 0x01. Caching
+
+Implementation of basic caching algorithms using Python
+
+## 0-basic_cache.py
+Implement a basic cache class that stores an unlimited number of items to
+an in-memory dictionary.
